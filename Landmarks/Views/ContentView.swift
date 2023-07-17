4b3970751807e622ed6203fa3e75cab945c68c24
aidanc2004 <67815343+aidanc2004@users.noreply.github.com>
2023-07-17 08:33:48 -0300

    Landmarks Swift Tutorial

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Aidan Carey on 2023-07-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
---
+++ Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -0,0 +1,66 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Aidan Carey on 2023-07-05.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
+    var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+    
+    var body: some View {
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
+                .frame(height: 300)
+                .ignoresSafeArea(edges: .top)
+            
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
+                
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About \(landmark.name)")
+                    .font(.title)
+                Text(landmark.description)
+            }
+            .padding()
+            
+            Spacer()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static let modelData = ModelData()
+    
+    static var previews: some View {
+        LandmarkDetail(landmark: ModelData().landmarks[0])
+            .environmentObject(modelData)
+    }
+}
